6bed8e8c6e42171bdfdf6437f3bae49ee51e3911
ckmuun <cornelius.koller@online.de>
2021-03-19 21:22:16 +0100

    add weightings for test strategy components

--- reccengine/engineImpl/FintoolRecomEngine_test.go
+++ reccengine/engineImpl/FintoolRecomEngine_test.go
@@ -63,20 +63,52 @@ func init() {
 
 func TestGenerateStrategyRecommendations(t *testing.T) {
 
-	strategyComponents := make([]api.StrategyComponent, 3)
+	strategyComponents := make([]api.StrategyComponent, 4)
 
 	component := api.StrategyComponent{
 		Description: "Just be a billionaire",
+		ScoreReqs: api.ScoreContainer{
+			Flexibility:         10,
+			Time:                10,
+			Intellectual:        10,
+			ExecutionComplexity: 10,
+			Risk:                10,
+		},
 	}
 	component1 := api.StrategyComponent{
 		Description: "donate everything and live in a barrell",
+		ScoreReqs: api.ScoreContainer{
+			Flexibility:         2,
+			Time:                2,
+			Intellectual:        3,
+			ExecutionComplexity: 4,
+			Risk:                6,
+		},
+	}
+	component3 := api.StrategyComponent{
+		Description: "collect social security",
+		ScoreReqs: api.ScoreContainer{
+			Flexibility:         1,
+			Time:                1,
+			Intellectual:        1,
+			ExecutionComplexity: 1,
+			Risk:                1,
+		},
 	}
 	component2 := api.StrategyComponent{
 		Description: "Just be a at least a millionaire",
+		ScoreReqs: api.ScoreContainer{
+			Flexibility:         5,
+			Time:                5,
+			Intellectual:        5,
+			ExecutionComplexity: 5,
+			Risk:                5,
+		},
 	}
 	strategyComponents[0] = component
 	strategyComponents[1] = component1
 	strategyComponents[2] = component2
+	strategyComponents[3] = component3
 
 	recommender := RuleBasedFintoolRecommender{
 		strategyComponents: strategyComponents,
