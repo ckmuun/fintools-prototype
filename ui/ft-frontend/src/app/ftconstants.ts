7c8e3ec089de6a3c39ce84d3273a27eada84cebb
ckmuun <cornelius.koller@online.de>
2021-06-24 13:22:43 +0200

    add typescript constants

---
+++ ui/ft-frontend/src/app/ftconstants.ts
@@ -0,0 +1,15 @@
+/*
+  type ScoreContainer struct {
+	FinRiskTolerance  int `json:"fin_risk_tolerance"`
+	PsyRiskTolerance  int `json:"psy_risk_tolerance"`
+	TimeFlexibility   int `json:"time_flexibility"`
+	CogBiasResistance int `json:"cog_bias_resistance"`
+	FinanceKnowledge  int `json:"finance_knowledge"`
+}
+ */
+import {ChartData, DefaultDataPoint} from "chart.js";
+import {FintoolRecom, ScoreContainer} from "./strategy.service";
+
+export const radarchartLabels: string[] = ['TimeFlexibility', '', 'Financial Risk Tolerance', 'Psychological Risk Tolerance', 'Cognitive Bias Resistance', 'Finance Knowledge'];
+
+
